07d4b241eef25df227f7e0f1037f9d5405aaae80
d2dwarf <d2dwarf@gmail.com>
2022-11-24 16:40:04 +0900

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
+export default App
